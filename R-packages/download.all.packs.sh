f2e25c294d085a2c4eefe3c3842b1874d9b29aeb
ramon diaz-uriarte <ramon@xantusia>
2007-05-04 16:44:40 +0200

    scripts for downloading and installing all BioC depends.

---
+++ R-packages/download.all.packs.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+## since BioC dependencies are such a pain, we first download all the required
+## (and maybe some extra stuff, but I no longer now).
+
+wget ftp://ftp.stat.math.ethz.ch/Software/CRAN/src/contrib/Rmpi*.gz
+wget ftp://ftp.stat.math.ethz.ch/Software/CRAN/src/contrib/papply*.gz
+wget ftp://ftp.stat.math.ethz.ch/Software/CRAN/src/contrib/sma*.gz 
+wget ftp://ftp.stat.math.ethz.ch/Software/CRAN/src/contrib/GDD*.gz
+wget ftp://ftp.stat.math.ethz.ch/Software/CRAN/src/contrib/gridBase*.gz
+wget ftp://ftp.stat.math.ethz.ch/Software/CRAN/src/contrib/colorspace*.gz 
+wget ftp://ftp.stat.math.ethz.ch/Software/CRAN/src/contrib/RColorBrewer*.gz 
+wget ftp://ftp.stat.math.ethz.ch/Software/CRAN/src/contrib/rlecuyer_*.gz
+wget ftp://ftp.stat.math.ethz.ch/Software/CRAN/src/contrib/zoo_*.gz
+wget ftp://ftp.stat.math.ethz.ch/Software/CRAN/src/contrib/sandwich_*.gz
+wget ftp://ftp.stat.math.ethz.ch/Software/CRAN/src/contrib/strucchange_*.gz
+wget ftp://ftp.stat.math.ethz.ch/Software/CRAN/src/contrib/cgh_*.gz 
+wget ftp://ftp.stat.math.ethz.ch/Software/CRAN/src/contrib/Hmisc_*.gz
+wget ftp://ftp.stat.math.ethz.ch/Software/CRAN/src/contrib/rsprng_*.gz
+wget ftp://ftp.stat.math.ethz.ch/Software/CRAN/src/contrib/combinat*.gz
+wget ftp://ftp.stat.math.ethz.ch/Software/CRAN/src/contrib/snow*.gz
+wget ftp://ftp.stat.math.ethz.ch/Software/CRAN/src/contrib/R2HTML*.gz
+wget ftp://ftp.stat.math.ethz.ch/Software/CRAN/src/contrib/aws*.gz
+wget ftp://ftp.stat.math.ethz.ch/Software/CRAN/src/contrib/pixmap_*.gz .
+wget ftp://ftp.stat.math.ethz.ch/Software/CRAN/src/contrib/waveslim_*.gz .
+wget ftp://ftp.stat.math.ethz.ch/Software/CRAN/src/contrib/xtable*.gz .
+
+
+
+rsync -rtlv --delete bioconductor.org::2.0/bioc/src/contrib/DynDoc*.gz .
+rsync -rtlv --delete bioconductor.org::2.0/bioc/src/contrib/widgetTools*.gz .
+rsync -rtlv --delete bioconductor.org::2.0/bioc/src/contrib/convert*.gz .
+rsync -rtlv --delete bioconductor.org::2.0/bioc/src/contrib/hexbin*.gz .
+rsync -rtlv --delete bioconductor.org::2.0/bioc/src/contrib/Biobase*.gz . 
+rsync -rtlv --delete bioconductor.org::2.0/bioc/src/contrib/Biostrings*.gz . 
+rsync -rtlv --delete bioconductor.org::2.0/bioc/src/contrib/affyio_*.gz .
+rsync -rtlv --delete bioconductor.org::2.0/bioc/src/contrib/affy_*.gz .
+rsync -rtlv --delete bioconductor.org::2.0/bioc/src/contrib/matchprobes_*.gz .
+rsync -rtlv --delete bioconductor.org::2.0/bioc/src/contrib/gcrma_*.gz .
+rsync -rtlv --delete bioconductor.org::2.0/bioc/src/contrib/affydata_*.gz .
+rsync -rtlv --delete bioconductor.org::2.0/bioc/src/contrib/affyPLM*.gz .
+rsync -rtlv --delete bioconductor.org::2.0/bioc/src/contrib/genefilter_*.gz .
+rsync -rtlv --delete bioconductor.org::2.0/bioc/src/contrib/limma_*.gz .
+rsync -rtlv --delete bioconductor.org::2.0/bioc/src/contrib/multtest*.gz .
+rsync -rtlv --delete bioconductor.org::2.0/bioc/src/contrib/marray_*.gz .
+rsync -rtlv --delete bioconductor.org::2.0/bioc/src/contrib/vsn_*.gz .
+rsync -rtlv --delete bioconductor.org::2.0/bioc/src/contrib/annotate_*.gz .
+rsync -rtlv --delete bioconductor.org::2.0/bioc/src/contrib/geneplotter_*.gz .
+rsync -rtlv --delete bioconductor.org::2.0/bioc/src/contrib/tkWidgets*.gz .
+rsync -rtlv --delete bioconductor.org::2.0/bioc/src/contrib/tilingArray_*.gz .
+rsync -rtlv --delete bioconductor.org::2.0/bioc/src/contrib/DNAcopy*.gz . 
+rsync -rtlv --delete bioconductor.org::2.0/bioc/src/contrib/aCGH_*.gz . 
+rsync -rtlv --delete bioconductor.org::2.0/bioc/src/contrib/arrayQuality_*gz .
+rsync -rtlv --delete bioconductor.org::2.0/bioc/src/contrib/cghMCR_*.gz . 
+rsync -rtlv --delete bioconductor.org::2.0/bioc/src/contrib/GLAD_*gz .
+rsync -rtlv --delete bioconductor.org::2.0/bioc/src/contrib/snapCGH_*gz .
+## I am tired. I get affydata by hand, copied from another dir
+
+
+
+
+
---
+++ R-packages/install.all.packs.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+#cd /http/R-custom/bin
+cd /home/sources.programs/R-patched/bin
+#cd /home/sources.programs/R-devel/bin
+
+
+./R CMD INSTALL ~/bzr-local-repos/adacgh-required-packs/aws*.gz
+./R CMD INSTALL ~/bzr-local-repos/adacgh-required-packs/Rmpi*.gz
+./R CMD INSTALL ~/bzr-local-repos/adacgh-required-packs/papply*.gz
+./R CMD INSTALL ~/bzr-local-repos/adacgh-required-packs/sma*.gz 
+./R CMD INSTALL ~/bzr-local-repos/adacgh-required-packs/GDD*.gz
+./R CMD INSTALL ~/bzr-local-repos/adacgh-required-packs/gridBase*.gz
+./R CMD INSTALL ~/bzr-local-repos/adacgh-required-packs/convert*.gz   
+./R CMD INSTALL ~/bzr-local-repos/adacgh-required-packs/colorspace*.gz 
+./R CMD INSTALL ~/bzr-local-repos/adacgh-required-packs/hexbin*.gz 
+./R CMD INSTALL ~/bzr-local-repos/adacgh-required-packs/RColorBrewer*.gz 
+./R CMD INSTALL ~/bzr-local-repos/adacgh-required-packs/rlecuyer_*.gz
+
+
+./R CMD INSTALL ~/bzr-local-repos/adacgh-required-packs/Biobase*.gz 
+./R CMD INSTALL ~/bzr-local-repos/adacgh-required-packs/DynDoc*.gz 
+./R CMD INSTALL ~/bzr-local-repos/adacgh-required-packs/widgetTools*.gz
+./R CMD INSTALL ~/bzr-local-repos/adacgh-required-packs/tkWidgets*.gz  
+./R CMD INSTALL ~/bzr-local-repos/adacgh-required-packs/Biostrings*.gz 
+./R CMD INSTALL ~/bzr-local-repos/adacgh-required-packs/affyio_*.gz
+./R CMD INSTALL ~/bzr-local-repos/adacgh-required-packs/affy_*.gz
+./R CMD INSTALL ~/bzr-local-repos/adacgh-required-packs/matchprobes_*.gz
+./R CMD INSTALL ~/bzr-local-repos/adacgh-required-packs/gcrma_*.gz
+./R CMD INSTALL ~/bzr-local-repos/adacgh-required-packs/affydata_*.gz
+./R CMD INSTALL ~/bzr-local-repos/adacgh-required-packs/affyPLM*.gz
+
+
+./R CMD INSTALL ~/bzr-local-repos/adacgh-required-packs/genefilter_*.gz
+./R CMD INSTALL ~/bzr-local-repos/adacgh-required-packs/limma_*.gz
+./R CMD INSTALL ~/bzr-local-repos/adacgh-required-packs/multtest*.gz
+./R CMD INSTALL ~/bzr-local-repos/adacgh-required-packs/marray_*.gz
+
+./R CMD INSTALL ~/bzr-local-repos/adacgh-required-packs/vsn_*.gz
+./R CMD INSTALL ~/bzr-local-repos/adacgh-required-packs/waveslim_*.gz
+./R CMD INSTALL ~/bzr-local-repos/adacgh-required-packs/xtable_*.gz
+
+./R CMD INSTALL ~/bzr-local-repos/adacgh-required-packs/zoo_*.gz
+./R CMD INSTALL ~/bzr-local-repos/adacgh-required-packs/sandwich_*.gz
+./R CMD INSTALL ~/bzr-local-repos/adacgh-required-packs/strucchange_*.gz
+
+./R CMD INSTALL ~/bzr-local-repos/adacgh-required-packs/annotate_*.gz
+./R CMD INSTALL ~/bzr-local-repos/adacgh-required-packs/geneplotter_*.gz
+./R CMD INSTALL ~/bzr-local-repos/adacgh-required-packs/pixmap_*.gz
+./R CMD INSTALL ~/bzr-local-repos/adacgh-required-packs/tilingArray_*.gz
+
+./R CMD INSTALL ~/bzr-local-repos/adacgh-required-packs/DNAcopy*.gz 
+./R CMD INSTALL ~/bzr-local-repos/adacgh-required-packs/cgh_*.gz 
+./R CMD INSTALL ~/bzr-local-repos/adacgh-required-packs/aCGH_*.gz 
+
+./R CMD INSTALL ~/bzr-local-repos/adacgh-required-packs/arrayQuality_*gz
+./R CMD INSTALL ~/bzr-local-repos/adacgh-required-packs/cghMCR_*.gz 
+
+./R CMD INSTALL ~/bzr-local-repos/adacgh-required-packs/GLAD_*gz
+./R CMD INSTALL ~/bzr-local-repos/adacgh-required-packs/snapCGH_*gz
+
+./R CMD INSTALL ~/bzr-local-repos/adacgh-required-packs/Hmisc_*.gz
+
+./R CMD INSTALL ~/bzr-local-repos/adacgh-required-packs/rsprng_*.gz
+
+./R CMD INSTALL ~/bzr-local-repos/adacgh-required-packs/combinat*.gz
+
+./R CMD INSTALL ~/bzr-local-repos/adacgh-required-packs/snow*.gz
+
+./R CMD INSTALL ~/bzr-local-repos/adacgh-required-packs/R2HTML*.gz
+
+
+
+
+
+
+

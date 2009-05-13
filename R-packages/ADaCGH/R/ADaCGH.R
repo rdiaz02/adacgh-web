41f43bc198436eaf46b739684c4fbb29b93c13e3
ramon diaz-uriarte <ramon@xantusia>
2009-05-13 19:21:34 +0200

    HaarSeg added; more robust against failures in other packages

--- R-packages/ADaCGH/R/ADaCGH.R
+++ R-packages/ADaCGH/R/ADaCGH.R
@@ -13,24 +13,43 @@ if(exists(".__ADaCGH_WEB_APPL", env = .GlobalEnv)) {
 
 
 
-names.formals.changepoints <- c("genomdat",
-                                "data.type",
-                                "alpha",
-                                "sbdry",
-                                "sbn",
-                                "nperm",
-                                "p.method",
-                                "min.width",
-                                "kmax",
-                                "nmin",
-                                "trimmed.SD",
-                                "undo.splits",
-                                "undo.prune",
-                                "undo.SD",
-                                "verbose",
-                                "ngrid",
-                                "tol")
+names.formals.changepoints.1.17 <- c("genomdat",
+                                     "data.type",
+                                     "alpha",
+                                     "sbdry",
+                                     "sbn",
+                                     "nperm",
+                                     "p.method",
+                                     "min.width",
+                                     "kmax",
+                                     "nmin",
+                                     "trimmed.SD",
+                                     "undo.splits",
+                                     "undo.prune",
+                                     "undo.SD",
+                                     "verbose",
+                                     "ngrid",
+                                     "tol")
+names.formals.changepoints.1.18 <- c("genomdat",
+                                     "data.type",
+                                     "alpha",
+                                     "weights",
+                                     "sbdry",
+                                     "sbn",
+                                     "nperm",
+                                     "p.method",
+                                     "min.width",
+                                     "kmax",
+                                     "nmin",
+                                     "trimmed.SD",
+                                     "undo.splits",
+                                     "undo.prune",
+                                     "undo.SD",
+                                     "verbose",
+                                     "ngrid",
+                                     "tol")
 
+## in v.1.18.0 we take advantage weights has default of NULL in changepoints
 
 vDNAcopy <- package_version(packageDescription("DNAcopy")$Version)
 if (vDNAcopy >= "1.17.1")
@@ -46,17 +65,33 @@ if (vDNAcopy >= "1.17.1")
     adacgh_trimmed.variance <- trimmed.variance
   }
 
+if(vDNAcopy >= "1.18.0") {
+  names.formals.changepoints <- names.formals.changepoints.1.18
+} else {
+  names.formals.changepoints <- names.formals.changepoints.1.17
+}
 
 if(!identical(names.formals.changepoints, names(formals(adacgh_changepoints)))) {
   m1 <- "Arguments to DNAcopy function changepoints have changed.\n"
   m2 <- "Either your version of DNAcopy is newer than ours, or older.\n"
-  m3 <- "If your version is different from 1.16.0 or 1.17.1,\n please let us know of this problem.\n"
-  m4 <- "We are assuming you are using DNAcopy version 1.16.0 or 1.17.1,\n"
-  m5 <- "the one for the current stable BioConductor release (v. 2.3)\n and the devel releas (v. 2.4).\n"
+  m3 <- "If your version is different from 1.16.0 or 1.17.1 or 1.18.0 or 1.19.0\n please let us know of this problem.\n"
+  m4 <- "We are assuming you are using DNAcopy version 1.16.0 or 1.17.1 or 1.18.0 or 1.19.0 ,\n"
+  m5 <- "the ones for the former  BioConductor release (v. 2.3), current stable release (v. 2.4)\n and the devel releas (v. 2.5).\n"
   m6 <- paste("Your version of DNAcopy is ", packageDescription("DNAcopy")$Version, ".\n")
   mm <- paste(m1, m2, m3, m4, m5, m6)
   stop(mm)
 }
+
+
+## As of v. 1.12 at least snapCGH finally has a namespace. So now we have
+## to do
+if(package_version(packageDescription("snapCGH")$Version) > "1.11")
+  myfit.model <- snapCGH:::fit.model
+## becasue even if fit.model is documented, it is NOT exported.
+## Well, they get away with it because there are no executable examples
+## in the help for fit.model.
+
+
 
 ## where do we live? to call the python script
 .python.toMap.py <- system.file("Python", "toMap.py", package = "ADaCGH")
@@ -99,6 +134,46 @@ mpiInit <- function(wdir = getwd(), minUniverseSize = 15,
     }
 }
 
+
+pSegmentHaarSeg <- function(x, chrom.numeric, m = 3,
+                            W = vector(),
+                            rawI = vector(), 
+                            breaksFdrQ = 0.001,			  
+                            haarStartLevel = 1,
+                            haarEndLevel = 5, ...) {
+  stop.na.inf(x)
+  stop.na.inf(chrom.numeric)
+  rle.chr <- rle(chrom.numeric)
+  chr.end <- cumsum(rle.chr$lengths)
+  chr.start <- c(1, chr.end[-length(chr.end)] + 1)
+  chromPos <- cbind(chr.start, chr.end)
+  out <- list()
+  out$segm <- list()
+##  dots <- as.list(substitute(list(...)))[-1]
+  
+  for(subj in 1:ncol(x)) {
+    cat("\n      running subject or column ", subj)
+    haarout <- ad_HaarSeg(x[, subj], chromPos = chromPos,
+                          W = W, rawI = rawI,
+                          breaksFdrQ = breaksFdrQ,
+                          haarStartLevel = haarStartLevel,
+                          haarEndLevel = haarEndLevel)[[2]]
+    mad.subj <- median(abs(x[, subj] - haarout))/0.6745
+    thresh <- m * mad.subj
+    ## alteration <- rep(0, nvalues)
+    ## alteration[haarout > thresh] <- 1
+    ## alteration[haarout < -thresh] <- -1
+    
+    out$segm[[subj]] <- cbind(Observed = x[, subj],
+                              Smoothed = haarout,
+                              Alteration =
+                              ifelse( abs(haarout > thresh), 1, 0) * sign(haarout))
+  }
+  cat("\n")
+  out$chrom.numeric <- chrom.numeric
+  class(out) <- c("adacgh.generic.out", "adacghHaarSeg")
+  return(out)
+}
 
 pSegmentACE <- function(x, chrom.numeric, parall = "auto", ...) {
   stop.na.inf(x)
@@ -217,11 +292,12 @@ pSegmentBioHMM_axc <- function(x, chrom.numeric, Pos, ...) {
                   function(z) BioHMMWrapper(z$logr, Pos = z$pos))
 
     te <- unlist(unlist(lapply(out0, function(x) inherits(x, "try-error"))))
+
     if(any(te)) {
         m1 <- "The BioHMM code occassionally crashes (don't blame us!)."
         m2 <- "You can try rerunning it a few times."
-        m3 <- "You can also tell the original authors that you get the error "
-        mm <- paste(m1, m2, m3, out0[[which(te)]])
+        m3 <- "You can also tell the original authors that you get the error(s): \n\n "
+        mm <- paste(m1, m2, m3, paste(out0[which(te)], collapse = "    \n   "))
         caughtError(mm)
     }
     matout0 <- matrix(unlist(out0), ncol = nsample)
@@ -759,7 +835,7 @@ SegmentPlotWrite <- function(data, chrom,
                                    Pos = Pos,
                                    mergeSegs = mergeSegs, ...)
                    )
-    seg1 <<- segmres
+##    seg1 <<- segmres
     if(inherits(trySegment, "try-error"))
         caughtOurError(trySegment)
     cat("\n\n Segmentation done \n\n")
@@ -956,6 +1032,14 @@ writeResults.CGHseg <- function(obj, acghdata, commondata,
                                 commondata, output = file,
                                  send_to_pals = FALSE)
 }
+
+writeResults.adacghHaarSeg <- function(obj, acghdata, commondata, 
+                                 file = "HaarSeg.output.txt", ...) {
+    print.adacgh.generic.results(obj, acghdata,
+                                 commondata, output = file)
+}
+
+
 
 writeResults.mergedHMM <- function(obj, acghdata, commondata, 
                                  file = "HMM.output.txt", ...) {
@@ -1160,7 +1244,7 @@ BioHMMWrapper <- function(logratio, Pos) {
     cat("\n       .... running BioHMMWrapper \n")
     ydat <- matrix(logratio, ncol=1)
     n <- length(ydat)
-    res <- try(fit.model(sample = 1, chrom = 1, dat = matrix(ydat, ncol = 1),
+    res <- try(myfit.model(sample = 1, chrom = 1, dat = matrix(ydat, ncol = 1),
                          datainfo = data.frame(Name = 1:n, Chrom = rep(1, n),
                          Position = Pos)))
     if(inherits(res, "try-error")) {
@@ -3496,6 +3580,8 @@ mapChromOpen <- function() {
     return(im2)
 }
 
+
+
 mapGenomeWideOpen <- function(main) {
     nameIm <- main
     imgheight <- 500
@@ -3781,7 +3867,8 @@ papply0 <- function (arg_sets, papply_action, papply_commondata = list(),
 
 
 
-mergeLevelsB <- function(vecObs,vecPred,pv.thres=0.0001,ansari.sign=0.05,thresMin=0.05,thresMax=0.5,verbose=1,scale=TRUE){
+mergeLevelsB <- function(vecObs, vecPred, pv.thres=0.0001, ansari.sign=0.05, thresMin=0.05,
+                         thresMax=0.5,verbose=1,scale=TRUE){
 
 # Check if supplied thresholds are valid
 if(thresMin>thresMax){cat("Error, thresMax should be equal to or larger than thresMin\n");return()}
@@ -3869,7 +3956,7 @@ while (1){
  }
 
 ### When done merging for a given threshold, test for significance ####
-        ansari[j]=ansari.test(sort(vecObs-vecPredNow), sort(vecObs-vecPred))$p.value
+        ansari[j]=my.ansari.test(sort(vecObs-vecPredNow), sort(vecObs-vecPred))$p.value
   if(is.na(ansari[j])){ansari[j]=0} # If too many numbers for test to be performed, a 0 is returned, resulting in no merging (please use fixed threshold to get any merging)
   lv[j]=length(mnNow) # get number of levels
 
@@ -3923,6 +4010,238 @@ while (1){
 # Return list of results
 return(list(vecMerged=vecPredNow,mnNow=mnNow,sq=sq,ansari=ansari))
 }
+
+
+
+
+
+my.ansari.test <- function (x, ...) {
+UseMethod("ansari.test")
+}
+
+my.ansari.test.default <-
+function (x, y, alternative = c("two.sided", "less", "greater"),
+exact = NULL, conf.int = FALSE, conf.level = 0.95, ...)
+{
+ alternative <- match.arg(alternative)
+ if (conf.int) {
+   if (!((length(conf.level) == 1) && is.finite(conf.level) &&
+     (conf.level > 0) && (conf.level < 1)))
+     stop("'conf.level' must be a single number between 0 and 1")
+ }
+ DNAME <- paste(deparse(substitute(x)), "and", deparse(substitute(y)))
+ x <- x[complete.cases(x)]
+ y <- y[complete.cases(y)]
+ m <- length(x)
+ if (m < 1)
+   stop("not enough 'x' observations")
+ n <- length(y)
+ if (n < 1)
+   stop("not enough 'y' observations")
+ N <- m + n
+ r <- rank(c(x, y))
+ STATISTIC <- sum(pmin(r, N - r + 1)[seq_along(x)])
+ TIES <- (length(r) != length(unique(r)))
+ if (is.null(exact))
+   exact <- ((m < 50) && (n < 50))
+ if (exact && !TIES) {
+   pansari <- function(q, m, n) {
+     .C(R_pansari, as.integer(length(q)), p = as.double(q),
+       as.integer(m), as.integer(n))$p
+   }
+   PVAL <- switch(alternative, two.sided = {
+     if (STATISTIC > ((m + 1)^2%/%4 + ((m * n)%/%2)/2))
+       p <- 1 - pansari(STATISTIC - 1, m, n)
+     else p <- pansari(STATISTIC, m, n)
+     min(2 * p, 1)
+   }, less = 1 - pansari(STATISTIC - 1, m, n), greater =
+pansari(STATISTIC,
+     m, n))
+   if (conf.int) {
+     qansari <- function(p, m, n) {
+       .C(R_qansari, as.integer(length(p)), q = as.double(p),
+        as.integer(m), as.integer(n))$q
+     }
+     alpha <- 1 - conf.level
+     x <- sort(x)
+     y <- sort(y)
+     ab <- function(sig) {
+       rab <- rank(c(x/sig, y))
+       sum(pmin(rab, N - rab + 1)[seq_along(x)])
+     }
+     ratio <- outer(x, y, "/")
+     aratio <- ratio[ratio >= 0]
+     sigma <- sort(aratio)
+     cci <- function(alpha) {
+       u <- absigma - qansari(alpha/2, m, n)
+       l <- absigma - qansari(1 - alpha/2, m, n)
+       uci <- NULL
+       lci <- NULL
+       if (length(u[u >= 0]) == 0 || length(l[l > 0]) ==
+        0) {
+        warning("samples differ in location: cannot compute
+confidence set, returning NA")
+        return(c(NA, NA))
+       }
+       if (is.null(uci)) {
+        u[u < 0] <- NA
+        uci <- min(sigma[which(u == min(u, na.rm = TRUE))])
+       }
+       if (is.null(lci)) {
+        l[l <= 0] <- NA
+        lci <- max(sigma[which(l == min(l, na.rm = TRUE))])
+       }
+       if (uci > lci) {
+        l <- absigma - qansari(alpha/2, m, n)
+        u <- absigma - qansari(1 - alpha/2, m, n)
+        u[u < 0] <- NA
+        uci <- min(sigma[which(u == min(u, na.rm = TRUE))])
+        l[l <= 0] <- NA
+        lci <- max(sigma[which(l == min(l, na.rm = TRUE))])
+       }
+       c(uci, lci)
+     }
+     cint <- if (length(sigma) < 1) {
+       warning("cannot compute confidence set, returning NA")
+       c(NA, NA)
+     }
+     else {
+       absigma <- sapply(sigma + c(diff(sigma)/2,
+sigma[length(sigma)] *
+        1.01), ab)
+       switch(alternative, two.sided = {
+        cci(alpha)
+       }, greater = {
+        c(cci(alpha * 2)[1], Inf)
+       }, less = {
+        c(0, cci(alpha * 2)[2])
+       })
+     }
+     attr(cint, "conf.level") <- conf.level
+     u <- absigma - qansari(0.5, m, n)
+     sgr <- sigma[u <= 0]
+     if (length(sgr) == 0)
+       sgr <- NA
+     else sgr <- max(sgr)
+     sle <- sigma[u > 0]
+     if (length(sle) == 0)
+       sle <- NA
+     else sle <- min(sle)
+     ESTIMATE <- mean(c(sle, sgr))
+   }
+ }
+ else {
+   EVEN <- ((N%%2) == 0)
+   normalize <- function(s, r, TIES, m = length(x), n = length(y)) {
+##########################################
+## Here is the problem: length(x) returns an integer
+m <- as.double(m)
+n <- as.double(n)
+##########################################
+     z <- if (EVEN)
+       s - m * (N + 2)/4
+     else s - m * (N + 1)^2/(4 * N)
+     if (!TIES) {
+       SIGMA <- if (EVEN)
+        sqrt((m * n * (N + 2) * (N - 2))/(48 * (N -
+         1)))
+       else sqrt((m * n * (N + 1) * (3 + N^2))/(48 *
+        N^2))
+     }
+     else {
+       r <- rle(sort(pmin(r, N - r + 1)))
+       SIGMA <- if (EVEN)
+        sqrt(m * n * (16 * sum(r$lengths * r$values^2) -
+         N * (N + 2)^2)/(16 * N * (N - 1)))
+       else sqrt(m * n * (16 * N * sum(r$lengths * r$values^2) -
+        (N + 1)^4)/(16 * N^2 * (N - 1)))
+     }
+     z/SIGMA
+   }
+   p <- pnorm(normalize(STATISTIC, r, TIES))
+   PVAL <- switch(alternative, two.sided = 2 * min(p, 1 -
+     p), less = 1 - p, greater = p)
+   if (conf.int && !exact) {
+     alpha <- 1 - conf.level
+     ab2 <- function(sig, zq) {
+       r <- rank(c(x/sig, y))
+       s <- sum(pmin(r, N - r + 1)[seq_along(x)])
+       TIES <- (length(r) != length(unique(r)))
+       normalize(s, r, TIES, length(x), length(y)) -
+        zq
+     }
+     srangepos <- NULL
+     srangeneg <- NULL
+     if (length(x[x > 0]) && length(y[y > 0]))
+       srangepos <- c(min(x[x > 0], na.rm = TRUE)/max(y[y >
+        0], na.rm = TRUE), max(x[x > 0], na.rm = TRUE)/min(y[y >
+        0], na.rm = TRUE))
+     if (length(x[x <= 0]) && length(y[y < 0]))
+       srangeneg <- c(min(x[x <= 0], na.rm = TRUE)/max(y[y <
+        0], na.rm = TRUE), max(x[x <= 0], na.rm = TRUE)/min(y[y <
+        0], na.rm = TRUE))
+     if (any(is.infinite(c(srangepos, srangeneg)))) {
+       warning("cannot compute asymptotic confidence set or estimator")
+       conf.int <- FALSE
+     }
+     else {
+       ccia <- function(alpha) {
+        statu <- ab2(srange[1], zq = qnorm(alpha/2))
+        statl <- ab2(srange[2], zq = qnorm(alpha/2,
+         lower.tail = FALSE))
+        if (statu > 0 || statl < 0) {
+         warning("samples differ in location: cannot compute confidence set, returning NA")
+         return(c(NA, NA))
+        }
+        u <- uniroot(ab2, srange, tol = 1e-04, zq =
+qnorm(alpha/2))$root
+        l <- uniroot(ab2, srange, tol = 1e-04, zq = qnorm(alpha/2,
+         lower.tail = FALSE))$root
+        sort(c(u, l))
+       }
+       srange <- range(c(srangepos, srangeneg), na.rm = FALSE)
+       cint <- switch(alternative, two.sided = {
+        ccia(alpha)
+       }, greater = {
+        c(ccia(alpha * 2)[1], Inf)
+       }, less = {
+        c(0, ccia(alpha * 2)[2])
+       })
+       attr(cint, "conf.level") <- conf.level
+       statu <- ab2(srange[1], zq = 0)
+       statl <- ab2(srange[2], zq = 0)
+       if (statu > 0 || statl < 0) {
+        ESTIMATE <- NA
+        warning("cannot compute estimate, returning NA")
+       }
+       else ESTIMATE <- uniroot(ab2, srange, tol = 1e-04,
+        zq = 0)$root
+     }
+   }
+   if (exact && TIES) {
+     warning("cannot compute exact p-value with ties")
+     if (conf.int)
+       warning("cannot compute exact confidence intervals with ties")
+   }
+ }
+ names(STATISTIC) <- "AB"
+ RVAL <- list(statistic = STATISTIC, p.value = PVAL, null.value =
+              c(`ratio of scales` = 1),
+   alternative = alternative, method = "Ansari-Bradley test",
+   data.name = DNAME)
+ if (conf.int)
+   RVAL <- c(RVAL, list(conf.int = cint, estimate = c(`ratio of scales` = ESTIMATE)))
+ class(RVAL) <- "htest"
+ return(RVAL)
+}
+
+
+
+
+
+
+
+
 
 #################################
 
@@ -4047,8 +4366,8 @@ pSegmentBioHMM_A <- function(x, chrom.numeric, Pos, ...) {
     if(any(te)) {
         m1 <- "The BioHMM code occassionally crashes (don't blame us!)."
         m2 <- "You can try rerunning it a few times."
-        m3 <- "You can also tell the original authors that you get the error "
-        mm <- paste(m1, m2, m3, out[[which(te)]])
+        m3 <- "You can also tell the original authors that you get the error(s): \n\n  "
+        mm <- paste(m1, m2, m3, paste(out[which(te)], collapse = "    \n   "))
         caughtError(mm)
     }
     outl <- list()
@@ -4067,7 +4386,7 @@ BioHMMWrapper_A <- function(logratio, Chrom, Pos) {
   for (ic in uchrom) {
       ydat <- logratio[Chrom == ic]
       n <- length(ydat)
-      res <- try(fit.model(sample = 1, chrom = ic, dat = matrix(ydat, ncol = 1),
+      res <- try(myfit.model(sample = 1, chrom = ic, dat = matrix(ydat, ncol = 1),
                        datainfo = data.frame(Name = 1:n, Chrom = rep(ic, n),
                        Position = Pos[Chrom == ic])))
       if(inherits(res, "try-error")) {

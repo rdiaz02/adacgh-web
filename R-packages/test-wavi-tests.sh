04a7652af56c0d40f20699f4c3d6dc7800760ae2
ramon diaz-uriarte <ramon@xantusia>
2009-07-13 12:15:08 +0200

    added test-wavi-tests.sh

---
+++ R-packages/test-wavi-tests.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+## Runs the testing code in wavi
+
+## You should have launched the LAM/MPI environment the way you
+## like it first.
+
+rm -r -f /tmp/wavi-tests
+mkdir /tmp/wavi-tests
+cp ../../adacgh-server/f1.R /tmp/wavi-tests/.
+
+cp -a ../../adacgh-server/test-cases/dnacopy-ok /tmp/wavi-tests/.
+cp -a ../../adacgh-server/test-cases/biohmm-ok /tmp/wavi-tests/.
+cp -a ../../adacgh-server/test-cases/hmm-ok /tmp/wavi-tests/.
+cp -a ../../adacgh-server/test-cases/glad-ok /tmp/wavi-tests/.
+cp -a ../../adacgh-server/test-cases/wavelets-ok /tmp/wavi-tests/.
+cp -a ../../adacgh-server/test-cases/cghseg-ok /tmp/wavi-tests/.
+cp -a ../../adacgh-server/test-cases/haarseg-ok /tmp/wavi-tests/.
+
+sed -i 's/.__ADaCGH_SERVER_APPL", TRUE)/.__ADaCGH_SERVER_APPL", FALSE)/' /tmp/wavi-tests/f1.R 
+
+cp /tmp/wavi-tests/f1.R /tmp/wavi-tests/dnacopy-ok/.
+cp /tmp/wavi-tests/f1.R /tmp/wavi-tests/biohmm-ok/.
+cp /tmp/wavi-tests/f1.R /tmp/wavi-tests/hmm-ok/.
+cp /tmp/wavi-tests/f1.R /tmp/wavi-tests/glad-ok/.
+cp /tmp/wavi-tests/f1.R /tmp/wavi-tests/wavelets-ok/.
+cp /tmp/wavi-tests/f1.R /tmp/wavi-tests/cghseg-ok/.
+cp /tmp/wavi-tests/f1.R /tmp/wavi-tests/haarseg-ok/.
+
+
+cd /tmp/wavi-tests
+cd dnacopy-ok
+R CMD BATCH f1.R
+cd ..
+cd biohmm-ok
+R CMD BATCH f1.R
+cd ..
+cd hmm-ok
+R CMD BATCH f1.R
+cd ..
+cd glad-ok
+R CMD BATCH f1.R
+cd ..
+cd wavelets-ok
+R CMD BATCH f1.R
+cd ..
+cd cghseg-ok
+R CMD BATCH f1.R
+cd ..
+cd haarseg-ok
+R CMD BATCH f1.R
+cd ..
+
+
+cat ./*/R_Status.txt

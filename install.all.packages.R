f0d4132ef3bb7f53dc632f004844d003a29161e9
Ramon Diaz Uriarte <ramon@Hyla>
2009-11-06 17:43:57 +0100

    install minor change

---
+++ install.all.packages.R
@@ -0,0 +1,14 @@
+## source this in R, and things should work
+
+source("http://bioconductor.org/biocLite.R")
+my.repos <- c("http://stat.ethz.ch/CRAN", biocinstallRepos())
+
+
+## or http://cran.ch.r-project.org/src/contrib/
+
+## we will change the call to ADaCGH; for now, that helps to
+## get the whole set of needed packages
+install.packages(c("ADaCGH", "snowfall", "Rmpi", "papply"), repos = my.repos)
+
+## when using a previously installed dir, such as binary, might need to use:
+# update.packages(repos = my.repos, ask = FALSE)
